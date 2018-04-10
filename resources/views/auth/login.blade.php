74e2f7df12b0f373b75d19d43a7f112638e02efa
Franco <franco.montti.19@gmail.com>
2018-04-10 18:01:29 -0300

    Primera subida app-shop

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('body-class','signup-page')
+
+@section('content')
+<div class="header header-filter" style="background-image: url('{{asset('/img/city.jpg')}}'); background-size: cover; background-position: top center;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+                <div class="card card-signup">
+                    <form class="form" method="POST" action="{{ route('login') }}">
+                         {{ csrf_field() }}
+
+
+                        <div class="header header-primary text-center">
+                            <h4>Sign Up</h4>
+                            <div class="social-line">
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-facebook-square"></i>
+                                </a>
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                                <a href="#pablo" class="btn btn-simple btn-just-icon">
+                                    <i class="fa fa-google-plus"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <p class="text-divider">Or Be Classical</p>
+                        <div class="content">
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">face</i>
+                                </span>
+                                <input type="text" class="form-control" placeholder="First Name...">
+                            </div>
+
+                            <div class="input-group {{ $errors->has('email') ? ' has-error' : '' }}">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">email</i>
+                                </span>
+                                <input type="text" class="form-control" placeholder="Email...">
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="material-icons">lock_outline</i>
+                                </span>
+                                <input type="password" placeholder="Password..." class="form-control" />
+                            </div>
+
+                            <!-- If you want to add a checkbox to this form, uncomment this code
+
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="optionsCheckboxes" checked>
+                                    Subscribe to newsletter
+                                </label>
+                            </div> -->
+                        </div>
+                        <div class="footer text-center">
+                            <a href="#pablo" class="btn btn-simple btn-primary btn-lg">Get Started</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <nav class="pull-left">
+                <ul>
+                    <li>
+                        <a href="#">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright pull-right">
+                &copy; 2016, made with <i class="fa fa-heart heart"></i> by <a href="#" target="_blank">Creative Tim</a>
+            </div>
+        </div>
+    </footer>
+
+</div>
+@endsection
